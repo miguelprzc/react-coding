6d27ec0342998b6644a3a9c353b3875eec8c5fda
miguelprzc <73727453+miguelprzc@users.noreply.github.com>
2021-01-17 23:06:53 -0400

    Create a simple App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const author = "Mike";
+const currentYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {author}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
